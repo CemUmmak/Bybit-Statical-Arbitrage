213b530ac4673a04f05b14577ae9473fc231a327
Cem Ummak <ummakcem@gmail.com>
2025-06-30 11:31:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-message = f"Orders closed by Z-Score.\n\nPNL : {152} Without commission.\n\nSpread :"+("Positive" if 15 >= 0 else "Negative")
+message = f"Orders closed by Z-Score.\n\nPNL : {152} Without commission.\nSpread :"+("Positive" if 15 >= 0 else "Negative")
 
 print(message)
